f72aa0fefa181d3f4e8a07fdccf2f0502c2cc034
g8208003-oss <g8208003@gmail.com>
2026-08-21 12:34:36 +0530

    Initial commit

---
+++ starter/answers.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT NOT NULL
+);
+
